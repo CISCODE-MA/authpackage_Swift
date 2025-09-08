f8676df5ed2d8859f95a06abcf8ce70b6c836dc5
Zaiidmo <zaiidmoumnii@gmail.com>
2025-09-08 13:09:18 +0100

    Create Login Service

--- Sources/AuthPackage/Data/Services/LoginService.swift
+++ Sources/AuthPackage/Data/Services/LoginService.swift
@@ -5,3 +5,38 @@
 //  Created by Zaid MOUMNI on 08/09/2025.
 //
 
+import Foundation
+
+public protocol LoginServicing {
+    func login(identifier: String, password: String, rememberMe: Bool)
+        async throws -> (
+            message: String?, user: User?, otpCode: String?, rememberMe: Bool?
+        )
+}
+
+public final class LoginService: LoginServicing {
+    private let config: AuthConfiguration
+    private let net: NetworkClient
+
+    public init(config: AuthConfiguration, net: NetworkClient) {
+        self.config = config
+        self.net = net
+    }
+
+    public func login(identifier: String, password: String, rememberMe: Bool)
+        async throws -> (
+            message: String?, user: User?, otpCode: String?, rememberMe: Bool?
+        )
+    {
+        let env: AuthEnvelope = try await net.send(
+            baseURL: config.baseURL, path: Endpoints.login, method: .POST,
+            headers: [:],
+            body: [
+                "identifier": identifier, "password": password,
+                "rememberMe": rememberMe,
+            ])
+
+        let user = env.user.map(Mapper.user)
+        return (env.message, user, env.otpCode?, env.rememberMe)
+    }
+}
