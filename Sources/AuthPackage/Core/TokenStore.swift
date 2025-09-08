d42a6007cca046ae536a5a9e5d9284234c5303c0
Zaiidmo <zaiidmoumnii@gmail.com>
2025-09-08 11:12:10 +0100

    Create and initiate the Token Storecore file

---
+++ Sources/AuthPackage/Core/TokenStore.swift
@@ -0,0 +1,36 @@
+//
+//  TokenStore.swift
+//  AuthPackage
+//
+//  Created by Zaid MOUMNI on 08/09/2025.
+//
+
+import Foundation
+
+public struct Tokens: Equatable, Sendable {
+    public let accessToken: String
+    public let refreshToken: String?
+    public let expiry: Date?
+
+    public init(
+        accessToken: String, refreshToken: String? = nil, expiry: Date? = nil
+    ) {
+        self.accessToken = accessToken
+        self.refreshToken = refreshToken
+        self.expiry = expiry
+    }
+
+    public protocol TokenStore {
+        func save(_ tokens: Tokens) throws
+        func load() throws -> Tokens?
+        func clear() throws
+    }
+
+    public final class InMemoryTokenStore: TokenStore {
+        private var box: Tokens?
+        public init() {}
+        public func save(_ tokens: Tokens) throws { box = tokens }
+        public func load() throws -> Tokens? { box }
+        public func clear() throws { box = nil }
+    }
+}
