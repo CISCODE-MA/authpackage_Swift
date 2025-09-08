22917f71df7d41e4a7337e2f2616a1c24d35fc0a
Zaiidmo <zaiidmoumnii@gmail.com>
2025-09-08 10:39:24 +0100

    Creating the AuthConfiguration file

--- Sources/AuthPackage/Core/AuthConfiguration.swift
+++ Sources/AuthPackage/Core/AuthConfiguration.swift
@@ -5,3 +5,15 @@
 //  Created by Zaid MOUMNI on 08/09/2025.
 //
 
+import Foundation
+
+public struct AuthConfiguration: Sendable {
+    public let baseURL: URL
+    /// If your backend sets refresh token in HttpOnly cookie, you can set this true.
+    public let refreshUsesCookie: Bool
+
+    public init(baseURL: URL, refreshUsesCookie: Bool = true) {
+        self.baseURL = baseURL
+        self.refreshUsesCookie = refreshUsesCookie
+    }
+}
