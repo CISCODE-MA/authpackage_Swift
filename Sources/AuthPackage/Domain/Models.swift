c2860f8640c781671766993bdb52beb6c9d85b10
Zaiidmo <zaiidmoumnii@gmail.com>
2025-09-08 11:20:53 +0100

    Create the Models File

--- Sources/AuthPackage/Domain/Models.swift
+++ Sources/AuthPackage/Domain/Models.swift
@@ -5,3 +5,15 @@
 //  Created by Zaid MOUMNI on 08/09/2025.
 //
 
+import Foundation
+
+public struct FullName: Codable, Equatable, Sendable { public let fname: String, public let lname: String}
+
+public struct User: Codable, Equatable, Sendable {
+    public let id: String?
+    public let fullname: FullName?
+    public let username: String
+    public let email: String
+    public let phoneNumber: String
+    public let roles: [String]
+}
