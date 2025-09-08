e2f1321a1b31894f17767a275af76c955ee01941
Zaiidmo <zaiidmoumnii@gmail.com>
2025-09-08 12:41:10 +0100

    Create EndPoints And Mappers files in the data directory

--- Sources/AuthPackage/Data/Endpoints.swift
+++ Sources/AuthPackage/Data/Endpoints.swift
@@ -5,3 +5,17 @@
 //  Created by Zaid MOUMNI on 08/09/2025.
 //
 
+import Foundation
+
+enum Endpoints {
+    // Build paths relative to baseURL from AuthConfiguration
+    static let login = "/auth/login"
+    static let verifyOTP = "/auth/verify-otp"
+    static let logout = "/auth/logout"
+    static let register = "/auth/register"
+    static let verifyEmail = "/auth/verify-email"
+    static let requestPasswordRest = "/auth/request-password-reset"
+    static let resetPassword = "/auth/reset-password"
+//    static let checkToken = "/verify/check-token"
+    static let refresh = "/auth/refresh"
+}
