e87c4ddb8c9d79dc6aefb440af8e31ae489bef7d
Zaiidmo <zaiidmoumnii@gmail.com>
2025-09-12 10:57:21 +0100

    Init the package UI-Module entry point

--- Sources/AuthPackageUI/AuthPackageUI.swift
+++ Sources/AuthPackageUI/AuthPackageUI.swift
@@ -5,3 +5,51 @@
 //  Created by Zaid MOUMNI on 12/09/2025.
 //
 
+@preconcurrency import AuthPackage
+import SwiftUI
+
+public enum AuthPackageUI {}
+
+extension AuthPackageUI {
+    @MainActor
+    public static func makeRoot(
+        config: AuthUIConfig,
+        client: AuthClientProtocol
+    ) -> some View {
+        _AuthUIRoot(config: config, client: client)
+    }
+}
+
+/// Private root view that configures the router in init (not inside a ViewBuilder)
+@MainActor
+private struct _AuthUIRoot: View {
+    let config: AuthUIConfig
+    @StateObject private var router = AuthUIRouter.shared
+
+    init(config: AuthUIConfig, client: AuthClientProtocol) {
+        self.config = config
+        AuthUIRouter.shared.configure(config: config, client: client)
+    }
+
+    @ViewBuilder
+    private var content: some View {
+        AuthFlowView()
+            .environmentObject(router)
+            .environment(\.authUIStyle, config.style)
+    }
+
+    var body: some View {
+        Group {
+            if #available(iOS 16.0, *) {
+                AnyView(
+                    NavigationStack { content }
+                )
+            } else {
+                AnyView(
+                    NavigationView { content }
+                        .navigationViewStyle(.stack)
+                )
+            }
+        }
+    }
+}
