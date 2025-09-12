25fbf9ffc26e89fe3f9131b2c5e4623dd26e3749
Zaiidmo <zaiidmoumnii@gmail.com>
2025-09-12 09:45:55 +0100

    Inject and import the New UI Module library into the packages Swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,8 @@ let package = Package(
     platforms: [.iOS(.v15), .macOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(name: "AuthPackage", targets: ["AuthPackage"])
+        .library(name: "AuthPackage", targets: ["AuthPackage"]),
+        .library(name: "AuthPackageUI", targets: ["AuthPackageUI"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -18,16 +19,20 @@ let package = Package(
             dependencies: [],
             path: "Sources/AuthPackage"
         ),
-
+        .target(
+            name: "AuthPackageUI",
+            dependencies: ["AuthPackage"],
+            path: "Sources/AuthPackageUI"
+        ),
         .testTarget(
             name: "AuthPackageTests",
             dependencies: ["AuthPackage"],
             path: "Tests/AuthPackageTests"
         ),
-       .testTarget(
-           name: "AuthPackageLiveTests",
-           dependencies: ["AuthPackage"]
-           
-       ),
+        .testTarget(
+            name: "AuthPackageLiveTests",
+            dependencies: ["AuthPackage"]
+
+        ),
     ]
 )
