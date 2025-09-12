8cb3f10df849a6a790db9c78041d4c3890f04780
Zaiidmo <zaiidmoumnii@gmail.com>
2025-09-12 10:57:37 +0100

    Create environment file for the UI-module;

--- Sources/AuthPackageUI/AuthUIEnvironment.swift
+++ Sources/AuthPackageUI/AuthUIEnvironment.swift
@@ -5,3 +5,19 @@
 //  Created by Zaid MOUMNI on 12/09/2025.
 //
 
+import SwiftUI
+
+private struct AuthUIStyleKey: EnvironmentKey {
+    static let defaultValue: AuthUIStyleSheet = .default
+}
+
+extension EnvironmentValues {
+    public var authUIStyle: AuthUIStyleSheet {
+        get {
+            self[AuthUIStyleKey.self]
+        }
+        set {
+            self[AuthUIStyleKey.self] = newValue
+        }
+    }
+}
