e44552c8075e4b7c8ba03f4170b84839b20c3ca9
Zaiidmo <zaiidmoumnii@gmail.com>
2025-09-17 16:21:56 +0100

    Update UI Configurations

--- Sources/AuthPackageUI/AuthUIConfig.swift
+++ Sources/AuthPackageUI/AuthUIConfig.swift
@@ -5,48 +5,54 @@
 //  Created by Zaid MOUMNI on 12/09/2025.
 //
 #if os(iOS)
+    import AuthPackage
+    import SwiftUI
 
-import AuthPackage
-import SwiftUI
+    public struct AuthUIConfig: Sendable {
+        public var baseURL: URL
+        public var appScheme: String
+        public var microsoftEnabled: Bool
+        public var googleEnabled: Bool  // NEW
+        public var facebookEnabled: Bool  // NEW
+        public var style: AuthUIStyleSheet
+        public var postLoginDeeplink: URL?
 
-public struct AuthUIConfig: Sendable {
-    public var baseURL: URL
-    public var appScheme: String
-    public var microsoftEnabled: Bool
-    public var style: AuthUIStyleSheet
-    public var postLoginDeeplink: URL?   // NEW
+        public init(
+            baseURL: URL,
+            appScheme: String,
+            microsoftEnabled: Bool = false,
+            googleEnabled: Bool = false,  // NEW
+            facebookEnabled: Bool = false,  // NEW
+            style: AuthUIStyleSheet = .default,
+            postLoginDeeplink: URL? = nil
+        ) {
+            self.baseURL = baseURL
+            self.appScheme = appScheme
+            self.microsoftEnabled = microsoftEnabled
+            self.googleEnabled = googleEnabled
+            self.facebookEnabled = facebookEnabled
+            self.style = style
+            self.postLoginDeeplink = postLoginDeeplink
+        }
 
-    // Primary initializer (keeps existing 'style' usage)
-    public init(
-        baseURL: URL,
-        appScheme: String,
-        microsoftEnabled: Bool = false,
-        style: AuthUIStyleSheet = .default,
-        postLoginDeeplink: URL? = nil
-    ) {
-        self.baseURL = baseURL
-        self.appScheme = appScheme
-        self.microsoftEnabled = microsoftEnabled
-        self.style = style
-        self.postLoginDeeplink = postLoginDeeplink
+        public init(
+            baseURL: URL,
+            appScheme: String,
+            microsoftEnabled: Bool = false,
+            googleEnabled: Bool = false,  // NEW
+            facebookEnabled: Bool = false,  // NEW
+            cssVariables: String,
+            postLoginDeeplink: URL? = nil
+        ) {
+            self.init(
+                baseURL: baseURL,
+                appScheme: appScheme,
+                microsoftEnabled: microsoftEnabled,
+                googleEnabled: googleEnabled,
+                facebookEnabled: facebookEnabled,
+                style: AuthUIStyleSheet.fromCSSVariables(cssVariables),
+                postLoginDeeplink: postLoginDeeplink
+            )
+        }
     }
-
-    // Convenience: build style from a CSS-like token string
-    public init(
-        baseURL: URL,
-        appScheme: String,
-        microsoftEnabled: Bool = false,
-        cssVariables: String,
-        postLoginDeeplink: URL? = nil
-    ) {
-        self.init(
-            baseURL: baseURL,
-            appScheme: appScheme,
-            microsoftEnabled: microsoftEnabled,
-            style: AuthUIStyleSheet.fromCSSVariables(cssVariables),
-            postLoginDeeplink: postLoginDeeplink
-        )
-    }
-}
 #endif
-
